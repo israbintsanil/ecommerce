3405cda82bcaef5b7068322596ee49f6b8d6064a
Your Name <your-email@example.com>
2026-07-27 11:06:46 +0530

    Update ALLOWED_HOSTS

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-d(m9=9ee+s#0=+@5w$e0f!#cxw=vm)ire7k$!z*^kwb4))!wyr
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [*]
 
 
 # Application definition
